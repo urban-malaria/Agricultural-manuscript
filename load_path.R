88a40f6a84a8d3a349ae399b139634b6492e08e8
Grace Legris <gracebea@gmail.com>
2025-06-20 09:18:43 -0400

    transferring data

---
+++ load_path.R
@@ -0,0 +1,116 @@
+
+
+# clear current workspace
+rm(list = ls())
+
+## =========================================================================================================================================
+### Directory Management and File Paths
+## =========================================================================================================================================
+
+user <- Sys.getenv("USER") 
+if ("ozodi" %in% user) {
+  Drive <- file.path(gsub("[\\]", "/", gsub("Documents", "", gsub("OneDrive", "", Sys.getenv("HOME")))))
+  Drive <- file.path(gsub("[//]", "/", Drive))
+  DriveDir <- file.path(Drive, "Urban Malaria Proj Dropbox", "urban_malaria")
+  PopDir <- file.path(DriveDir, "data", "data_agric_analysis")
+  ManDir <- file.path(DriveDir, "projects", "Manuscripts", "ongoing", "agriculture_malaria_manuscript")
+  FigDir <- file.path(DriveDir, "projects", "Manuscripts", "agriculture_malaria_manuscript", "figures", "220623_new_figures")
+} else if ("CHZCHI003" %in% user) {
+  Drive <- file.path("C:/Users/CHZCHI003/OneDrive")
+  DriveDir <- file.path(Drive, "urban_malaria")
+  PopDir <- file.path(DriveDir, "data", 'Urban_malaria_net_ownership_data')
+  ManDir <- file.path(DriveDir, "projects", "Manuscripts", "agriculture_malaria_manuscript")
+  FigDir <- file.path(ManDir, "figures", "220623_new_figures")
+} else if ("grace" %in% user) {
+  Drive <- "/Users/grace/Urban Malaria Proj Dropbox"
+  DriveDir <- file.path(Drive, "urban_malaria")
+  PopDir <- file.path(DriveDir, "data", 'data_agric_analysis')
+  ManDir <- file.path(DriveDir, "projects", "Manuscripts", "ongoing", "agriculture_malaria_manuscript")
+  FigDir <- file.path(ManDir, "figures", "exploratory")
+}else {
+  Drive <- file.path(gsub("[\\]", "/", gsub("Documents", "", Sys.getenv("HOME"))))
+  DriveDir <- file.path(Drive, "OneDrive - Northwestern University", "urban_malaria")
+  #DriveDir <- file.path(user_path, "OneDrive - Northwestern University", "urban_malaria")
+  #DriveDir <- file.path(Drive, '', 'Northwestern University', 'Ifeoma Doreen Ozodiegwu - urban_malaria')
+  #DriveDir <- file.path(Drive, '', 'Library', 'CloudStorage', "OneDrive-NorthwesternUniversity", "urban_malaria")
+  DriveDir <- file.path(Drive, "OneDrive - Personal", "urban_malaria")
+  PopDir <- file.path(DriveDir, "data", 'Urban_malaria_net_ownership_data')
+  ManDir <- file.path(DriveDir, "projects", "Manuscripts", "agriculture_malaria_manuscript")
+  FigDir <- file.path(ManDir, "figures", "220623_new_figures")
+}
+
+if ("ozodi" %in% user) {
+  Drive <- file.path(gsub("[\\]", "/", gsub("Documents", "", gsub("OneDrive", "", Sys.getenv("HOME")))))
+  Drive <- file.path(gsub("[//]", "/", Drive))
+  DriveDir <- file.path(Drive, "Urban Malaria Proj Dropbox", "urban_malaria")
+  PopDir <- file.path(DriveDir, "data", "data_agric_analysis")
+  ManDir <- file.path(DriveDir, "projects", "Manuscripts","ongoing", "agriculture_malaria_manuscript")
+  FigDir <- file.path(ManDir, "figures", "exploratory")
+} else if  ("CHZCHI003" %in% user) {
+  Drive <- file.path("C:/Users/CHZCHI003/OneDrive")
+  DriveDir <- file.path(Drive, "urban_malaria")
+  PopDir <- file.path(DriveDir, "data", 'data_agric_analysis')
+  ManDir <- file.path(DriveDir, "projects", "Manuscripts", "agriculture_malaria_manuscript")
+  FigDir <- file.path(ManDir, "figures", "exploratory")
+} else {
+  Drive <- file.path(gsub("[\\]", "/", gsub("Documents", "", Sys.getenv("HOME"))))
+  DriveDir <- file.path(Drive, 'Library', 'CloudStorage', 'OneDrive-NorthwesternUniversity', "urban_malaria")
+  #DriveDir <- file.path(Drive,  "OneDrive - Northwestern University", "urban_malaria")
+  PopDir <- file.path(DriveDir, "data", 'data_agric_analysis')
+  ManDir <- file.path(DriveDir, "projects", "Manuscripts", "agriculture_malaria_manuscript")
+  FigDir <- file.path(ManDir, "figures", "exploratory")
+}
+
+user <- Sys.getenv("USER")
+if ("ozodi"  %in% user) {
+  Drive <- file.path(gsub("[\\]", "/", gsub("Documents", "", gsub("OneDrive", "", Sys.getenv("HOME")))))
+  Drive <- file.path(gsub("[//]", "/", Drive))
+  DriveDir <- file.path(Drive, "Urban Malaria Proj Dropbox", "urban_malaria")
+  PopDir <- file.path(DriveDir, "data", "data_agric_analysis")
+  ManDir <- file.path(DriveDir, "projects", "Manuscripts", "ongoing", "agriculture_malaria_manuscript")
+  FigDir <- file.path(ManDir, "figures", "220623_new_figures")
+  EviDir <- file.path(PopDir, "MAP_EVI_monthly")
+  PrecDir <- file.path(PopDir, "chirps_monthly_precip")
+  HumDir <- file.path(PopDir, "ERA5_rel_humidity_monthly")
+  TempDir <- file.path(PopDir, "ERA5_temperature")
+  OutDir <- file.path(PopDir, "analysis_dat")
+} else if  ("CHZCHI003" %in% user) {
+  Drive <- file.path("C:/Users/CHZCHI003/OneDrive")
+  DriveDir <- file.path(Drive, "urban_malaria")
+  PopDir <- file.path(DriveDir, "data", 'data_agric_analysis')
+  ManDir <- file.path(DriveDir, "projects", "Manuscripts", "agriculture_malaria_manuscript")
+  FigDir <- file.path(ManDir, "figures", "220623_new_figures")
+  EviDir <- file.path(PopDir, "MAP_EVI_monthly")
+  PrecDir <- file.path(PopDir, "chirps_monthly_precip")
+  HumDir <- file.path(PopDir, "ERA5_rel_humidity_monthly")
+  TempDir <- file.path(PopDir, "ERA5_temperature")
+  OutDir <- file.path(PopDir, "analysis_dat")
+} else if ("grace" %in% user) {
+  Drive <- "/Users/grace/Urban Malaria Proj Dropbox"
+  DriveDir <- file.path(Drive, "urban_malaria")
+  PopDir <- file.path(DriveDir, "data", 'data_agric_analysis')
+  ManDir <- file.path(DriveDir, "projects", "Manuscripts", "ongoing", "agriculture_malaria_manuscript")
+  FigDir <- file.path(ManDir, "figures", "exploratory")
+  EviDir <- file.path(PopDir, "MAP_EVI_monthly")
+  PrecDir <- file.path(PopDir, "chirps_monthly_precip")
+  HumDir <- file.path(PopDir, "ERA5_rel_humidity_monthly")
+  TempDir <- file.path(PopDir, "ERA5_temperature")
+  OutDir <- file.path(PopDir, "analysis_dat")
+} else {
+  Drive <- file.path(gsub("[\\]", "/", gsub("Documents", "", Sys.getenv("HOME"))))
+  DriveDir <- file.path(Drive, 'Library', 'CloudStorage', 'OneDrive-NorthwesternUniversity', "urban_malaria")
+  #DriveDir <- file.path(Drive,  "OneDrive - Northwestern University", "urban_malaria")
+  PopDir <- file.path(DriveDir, "data", 'data_agric_analysis')
+  ManDir <- file.path(DriveDir, "projects", "Manuscripts", "agriculture_malaria_manuscript")
+  FigDir <- file.path(ManDir, "figures", "220623_new_figures")
+  EviDir <- file.path(PopDir, "MAP_EVI_monthly")
+  PrecDir <- file.path(PopDir, "chirps_monthly_precip")
+  HumDir <- file.path(PopDir, "ERA5_rel_humidity_monthly")
+  TempDir <- file.path(PopDir, "ERA5_temperature")
+  OutDir <- file.path(PopDir, "analysis_dat")
+}
+
+library(rdhs)
+
+source("functions/functions_employment.R")
+options(survey.lonely.psu="adjust")
